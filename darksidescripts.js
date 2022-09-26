3997d75637f9b260182c81c070805d0b3e47ab60
AloanScripting <99504478+AloanScripting@users.noreply.github.com>
2022-09-26 18:11:52 -0400

    Add files via upload

---
+++ darksidescripts.js
@@ -0,0 +1,8 @@
+//ionos hosted functions
+
+function changeSportsURL() {
+    const elements = document.getElementsByClassName('header__logo-link sports-cards ');
+    for (const element of elements) {
+        element.href = "https://darksidegames.com/collections/sports-cards";
+    }
+} window.onload=changeSportsURL()
