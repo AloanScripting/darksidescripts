d3d68537944566fa2f091e4000a0b6564810536e
AloanScripting <99504478+AloanScripting@users.noreply.github.com>
2022-09-26 18:28:08 -0400

    Update darksidescripts.js

--- darksidescripts.js
+++ darksidescripts.js
@@ -4,5 +4,5 @@ function changeSportsURL() {
     const elements = document.getElementsByClassName('header__logo-link sports-cards ');
     for (const element of elements) {
         element.href = "https://darksidegames.com/collections/sports-cards";
-    }
-} changeSportsURL()
+    };
+}; window.onload = changeSportsURL();
