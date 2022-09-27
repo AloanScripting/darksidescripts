645fe562da0b30c88dcf8bdff06bfdc90fc96eb1
dj <aloanscripting@gmail.com>
2022-09-26 22:05:14 -0400

    annoying

--- darksidescriptsv1.js
+++ darksidescriptsv1.js
@@ -39,11 +39,11 @@ function createElementsIDChecker() {
 } window.onload = createElementsIDChecker()
 
 function createElementsClassChecker() { 
-  const bodyID = document.getElementsByTagName('body')[-0].id;
+  const headerID = document.getElementsByTagName('header')[-0].id;
   const classMatcher = document.querySelectorAll(
   ".binderpos-credit-input-div,.form__input-row,.quantity-selector__button,.product-recommendations,.product__refund-policy-link,.product-item__title text--strong link, .mobile-menu__back-button, .card__title, .mobile-menu__section,.header__inner,.header__action-item-title, .product-item__action-button,.productCard__imageWrap, .nav-dropdown__link, .select-wrapper--primary, .select-wrapper,.product-item, .product-item__info-inner, .section__header, .mobile-menu__inner, .mega-menu__column, .header__mobile-nav-toggle, .nav-bar, .nav-bar__inner, .nav-bar__item, .product-meta__vendor, .product-meta,.product-meta__title, .section__title, .product-form__info-title, .product-form__info-item, .shipping-estimator, .button-group, .button, .heading, .mini-cart__product-info, .productCard__button--outOfStock, .product-meta__title, .header__logo-image,.announcement-bar,.footer__title, .header__cart-count");
   // Loop through all elements (including html, head, meta, scripts) and add collection.url
   for (const classElement of classMatcher) {
-  classElement.id = 'collections-'+bodyID;
+  classElement.id = headerID;
 } 
 } window.onload = createElementsClassChecker; 
