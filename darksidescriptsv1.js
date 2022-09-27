6f5230b63dec7e64d6e1e58b1451abdde38af1eb
dj <aloanscripting@gmail.com>
2022-09-27 00:15:18 -0400

    ay

--- darksidescriptsv1.js
+++ darksidescriptsv1.js
@@ -1,6 +1,6 @@
 //DS Functions
 
-function changeSportsURL() {
+  function changeSportsURL() {
     const elements = document.getElementsByClassName('header__logo-link sports-cards ');
     for (const element of elements) {
         element.href = "https://darksidegames.com/collections/sports-cards";
@@ -17,9 +17,9 @@ function changeSportsURL() {
 };  
 
 
-function executeFancyBox() {
-  if (window.location.pathname.length < 2){
-  const fancybox = new Fancybox([
+  function executeFancyBox() {
+    if (window.location.pathname.length < 2){
+    const fancybox = new Fancybox([
     {
       src: "<img><h3>Discount Code:</h2><h3>Darkside10</h2><p>Valid on MTG or Pokemon singles purchases over $10.00! (Cannot be combined with store credit.)</p>",
       type: "html",
@@ -34,32 +34,26 @@ function executeFancyBox() {
 function createElementsIDChecker() { 
   const bodyID = document.getElementsByTagName('body')[-0].id;
   const idMatcher = document.querySelectorAll(
-".header__logo-link,#shopify-section-template--15302417645613__content");
+  ".header__logo-link,#shopify-section-template--15302417645613__content"
+  );
 // Loop through all elements (including html, head, meta, scripts) and add collection.url
   for (const idElement of idMatcher) {
-  idElement.classList.add(bodyID)
-} 
-} 
+    idElement.classList.add(bodyID)
+  }; 
+} ;
 
 
 function createElementsClassChecker() { 
-  const htmlID = document.getElementsByTagName('html')[-0].id;
-  const classMatcher = document.querySelectorAll(
-  ".product-form__payment-container,.binderpos-credit-input-div,.form__input-row,.quantity-selector__button,.product-recommendations,.product__refund-policy-link,.product-item__title text--strong link, .mobile-menu__back-button, .card__title, .mobile-menu__section,.header__inner,.header__action-item-title, .product-item__action-button,.productCard__imageWrap, .nav-dropdown__link, .select-wrapper--primary, .select-wrapper,.product-item, .product-item__info-inner, .section__header, .mobile-menu__inner, .mega-menu__column, .header__mobile-nav-toggle, .nav-bar, .nav-bar__inner, .nav-bar__item, .product-meta__vendor, .product-meta,.product-meta__title, .section__title, .product-form__info-title, .product-form__info-item, .shipping-estimator, .button-group, .button, .heading, .mini-cart__product-info, .productCard__button--outOfStock, .product-meta__title, .header__logo-image,.announcement-bar,.footer__title, .header__cart-count");
-  // Loop through all elements (including html, head, meta, scripts) and add collection.url
-  for (const classElement of classMatcher) {
-  classElement.id = htmlID
-} 
-} 
+    const htmlID = document.getElementsByTagName('html')[-0].id;
+    const classMatcher = document.querySelectorAll(
+    ".product-form__payment-container,.binderpos-credit-input-div,.form__input-row,.quantity-selector__button,.product-recommendations,.product__refund-policy-link,.product-item__title text--strong link, .mobile-menu__back-button, .card__title, .mobile-menu__section,.header__inner,.header__action-item-title, .product-item__action-button,.productCard__imageWrap, .nav-dropdown__link, .select-wrapper--primary, .select-wrapper,.product-item, .product-item__info-inner, .section__header, .mobile-menu__inner, .mega-menu__column, .header__mobile-nav-toggle, .nav-bar, .nav-bar__inner, .nav-bar__item, .product-meta__vendor, .product-meta,.product-meta__title, .section__title, .product-form__info-title, .product-form__info-item, .shipping-estimator, .button-group, .button, .heading, .mini-cart__product-info, .productCard__button--outOfStock, .product-meta__title, .header__logo-image,.announcement-bar,.footer__title, .header__cart-count");
+    // Loop through all elements (including html, head, meta, scripts) and add collection.url
+    for (const classElement of classMatcher) {
+    classElement.id = htmlID
+  };
+}; 
 
-function getMySha() {
-var git = require('git-last-commit');
 
-git.getLastCommit(function(err, commit) {
-  // read commit object properties
-  console.log(commit);
-}); 
-}window.onload = getMySha() 
 
 
 
