da412227f2823bc151a5b885cf6ca49e92737378
dj <aloanscripting@gmail.com>
2022-10-23 17:01:55 -0400

    round 1

--- darksidescriptsv1.js
+++ darksidescriptsv1.js
@@ -24,12 +24,12 @@
 
 
 //search page sports for param setter
-(function($) {
-  let darksideSportsUrl = $(location).attr('href');
-  if(darksideSportsUrl.indexOf('search?page') != -1 && ($('.productCard__card ').data('producttype','Sports'))) {
-  $('html').attr( 'id', "collections-sports-cards");
-  }
-})(jQuery);
+// (function($) {
+//   let darksideSportsUrl = $(location).attr('href');
+//   if ($('.productCard__card ').data('producttype','Sports') && darksideSportsUrl.indexOf('search?page') != -1 ) {
+//   $('html').attr( 'id', "collections-sports-cards");
+//   }
+// })(jQuery);
 
 //param setter
 $(document).ready(function() {
